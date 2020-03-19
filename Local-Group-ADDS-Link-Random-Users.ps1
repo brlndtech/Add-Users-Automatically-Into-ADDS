419bf0422cb7cd6b27673473e8f539760668f506
brlndtech <itsgeoffreyberland@gmail.com>
2020-03-19 11:14:02 +0100

    modification des deux fichiers GRP
    concernant les groupes de domaine locaux. (optimisation ...)

--- Local-Group-ADDS-Link-Random-Users.ps1
+++ Local-Group-ADDS-Link-Random-Users.ps1
@@ -1,4 +1,12 @@
-﻿<#
+﻿##### 
+if (Get-Module -ListAvailable -Name NTFSSecurity) {
+    Write-Output "Module NTFSSecurity exists"
+} 
+else {
+    Write-Output "Module does not exist, If you ware connected to the internet, I will try to downoad and install the following module : NTFSSecurity"
+    Install-Module NTFSSecurity
+}
+<#
 Exemple d'arborescence pour le site de Paris 
 - Partage
     - Sites 
@@ -23,28 +31,18 @@ $sites = @("$site1", "$site2", "$site3") # villes
 $services = @("$service1","$service2","$service3") #services 
 $folders = @("Commun", "Data") # shared folder into services/ direction/ ... 
 # $subfolder = @("Commun", "Data")
-$rightTypes = @("CT", "LM", "R") # permissions 
-
-$answer=Read-Host "L'aborescence du script ADDS Add Random User a été crée  ? (O/N)"
-If ($answer -eq "N") {
-
-    New-ADOrganizationalUnit -Name "Sites" -Path "dc=$netbios,dc=$domain"
-    New-ADOrganizationalUnit -Name "Groupes" -Path "ou=Sites,dc=$netbios,dc=$domain"
-    New-ADOrganizationalUnit -Name "Globaux" -Path "ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
-    New-ADOrganizationalUnit -Name "Locaux" -Path "ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
-
-}
+$rightTypes = @("CT", "LM", "R") # permissions
 
 foreach ($partage in $partages) 
 {
-    New-Item -path "c:\" -Name "Partage" -ItemType directory # création du dossier Partage dans c:\
+    New-Item -path "c:\" -Name "Partage" -ItemType directory | Out-Null # création du dossier Partage dans c:\
+    
 
     foreach($rightType in $rightTypes)
     {
-       $groupName0 = "DL-$($partage)-$($rootSites)-$($rightType)"
+       $groupName0 = "DL-$($partage)-$($rightType)"
        Write-Host $groupName0
        New-ADGroup -Name "$groupName0" -GroupScope DomainLocal -GroupCategory Security -Path "ou=Locaux,ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
-       Write-Host "Creating $($groupName0)"
     }
 }
 
@@ -52,14 +50,13 @@ foreach ($partage in $partages)
 {
     foreach ($rootSite in $rootSites) 
     {
-        New-Item -path "c:\Partage\" -Name "Sites" -ItemType directory # création du sous dossier Sites dans c:\
-
+        New-Item -path "c:\Partage\" -Name "Sites" -ItemType directory | Out-Null # création du sous dossier Sites dans c:\
+        
         foreach($rightType in $rightTypes)
         {
-           $groupName0 = "DL-$($partage)-$($rightType)"
+           $groupName0 = "DL-$($partage)-$($rootSites)-$($rightType)"
            Write-Host $groupName0
            New-ADGroup -Name "$groupName0" -GroupScope DomainLocal -GroupCategory Security -Path "ou=Locaux,ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
-           Write-Host "Creating $($groupName0)"
         }
     }
 }
@@ -70,14 +67,14 @@ foreach ($partage in $partages)
     {
         foreach ($site in $sites) # DL-Partage-Site-Paris-CT LM L
         {
-            New-Item -path "c:\Partage\Sites" -Name "$site" -ItemType directory # création des sous dossier dans c:\
+            New-Item -path "c:\Partage\Sites" -Name "$site" -ItemType directory | Out-Null # création des sous dossier dans c:\
+            
 
             foreach($rightType in $rightTypes)
             {
                $groupName1 = "DL-$($partage)-$($rootSite)-$($site)-$($rightType)"
                Write-Host $groupName1
                New-ADGroup -Name "$groupName1" -GroupScope DomainLocal -GroupCategory Security -Path "ou=Locaux,ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
-               Write-Host "Creating $($groupName1)"
             }
         }
     }
@@ -92,14 +89,14 @@ foreach ($partage in $partages)
         {
             foreach($service in $services)
             {
-                New-Item -path "c:\Partage\Sites\$site" -Name "$service" -ItemType directory # création des sous dossier dans c:\
+                New-Item -path "c:\Partage\Sites\$site" -Name "$service" -ItemType directory | Out-Null # création des sous dossier dans c:\
+             
 
                 foreach($rightType in $rightTypes)
                 {
                    $groupName2 = "DL-$($partage)-$($rootSite)-$($site)-$($service)-$($rightType)"
                    Write-Host $groupName2
                    New-ADGroup -Name "$groupName2" -GroupScope DomainLocal -GroupCategory Security -Path "ou=Locaux,ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
-                   Write-Host "Creating $($groupName2)"
                }
             }
         }
@@ -117,14 +114,13 @@ foreach ($partage in $partages)
                 foreach($folder in $folders)
                 {
 
-                New-Item -path "c:\Partage\Sites\$site\$service" -Name "$folder" -ItemType directory # création des sous dossier dans c:\
+                New-Item -path "c:\Partage\Sites\$site\$service" -Name "$folder" -ItemType directory | Out-Null # création des sous dossier dans c:\
 
                     foreach($rightType in $rightTypes)
                     {
                         $groupName3 = "DL-$($partage)-$($rootSite)-$($site)-$($service)-$($folder)-$($rightType)"
                         Write-Host $groupName3
                         New-ADGroup -Name "$groupName3" -GroupScope DomainLocal -GroupCategory Security -Path "ou=Locaux,ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
-                        Write-Host "Creating $($groupName3)"
                     }
                 }
             }
--- Standalone-Group-ADDS-Link-Random-Users.ps1
+++ Standalone-Group-ADDS-Link-Random-Users.ps1
@@ -1,4 +1,6 @@
-﻿<#
+﻿
+Clear-Host
+<#
 Exemple d'arborescence pour le site de Paris 
 - Partage
     - Sites 
@@ -16,6 +18,13 @@ Exemple d'arborescence pour le site de Paris
         - Marseille (...)
 
 #>
+if (Get-Module -ListAvailable -Name NTFSSecurity) {
+    Write-Output " // Module NTFSSecurity exists // "
+} 
+else {
+    Write-Output " // Module does not exist, If you ware connected to the internet, I will try to downoad and install the following module : NTFSSecurity // "
+    Install-Module NTFSSecurity
+}
 # Création de trois groupe de domaines globaux avec suffixage  CT,LM,L pour chaque dossiers. 
 $partages = @("Partage")
 $rootSites = @("Sites")
@@ -27,23 +36,20 @@ $rightTypes = @("CT", "LM", "R") # permissions
 
 $netbios=Read-Host "Entrez le NetBIOS de votre domaine (ex : entreprise) "
 $domain=Read-Host "Entrez votre .tld (ex : .com) "
-$answer=Read-Host "L'aborescence du script ADDS Add Random User a été crée  ? (O/N)"
-If ($answer -eq "N") {
 
-    New-ADOrganizationalUnit -Name "Sites" -Path "dc=$netbios,dc=$domain"
-    New-ADOrganizationalUnit -Name "Groupes" -Path "ou=Sites,dc=$netbios,dc=$domain"
-    New-ADOrganizationalUnit -Name "Globaux" -Path "ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
-    New-ADOrganizationalUnit -Name "Locaux" -Path "ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
+New-ADOrganizationalUnit -Name "Sites" -Path "dc=$netbios,dc=$domain"
+New-ADOrganizationalUnit -Name "Groupes" -Path "ou=Sites,dc=$netbios,dc=$domain"
+New-ADOrganizationalUnit -Name "Globaux" -Path "ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
+New-ADOrganizationalUnit -Name "Locaux" -Path "ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
 
-}
 
 foreach ($partage in $partages) 
 {
-    New-Item -path "c:\" -Name "Partage" -ItemType directory # création du dossier Partage dans c:\
-
+    New-Item -path "c:\" -Name "Partage" -ItemType directory | Out-Null # création du dossier Partage dans c:\
+    #New-SmbShare -Name Partage -Path C:\Partage -ChangeAccess "Authentificated Users" -Description "Accès racine pour le partage"
     foreach($rightType in $rightTypes)
     {
-       $groupName0 = "DL-$($partage)-$($rootSites)-$($rightType)"
+       $groupName0 = "DL-$($partage)-$($rightType)"
        Write-Host $groupName0
        New-ADGroup -Name "$groupName0" -GroupScope DomainLocal -GroupCategory Security -Path "ou=Locaux,ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
        Write-Host "Creating $($groupName0)"
@@ -54,14 +60,14 @@ foreach ($partage in $partages)
 {
     foreach ($rootSite in $rootSites) 
     {
-        New-Item -path "c:\Partage\" -Name "Sites" -ItemType directory # création du sous dossier Sites dans c:\
+        New-Item -path "c:\Partage\" -Name "Sites" -ItemType directory | Out-Null # création du sous dossier Sites dans c:\
 
         foreach($rightType in $rightTypes)
         {
-           $groupName0 = "DL-$($partage)-$($rightType)"
+           $groupName0 = "DL-$($partage)-$($rootSites)-$($rightType)"
            Write-Host $groupName0
            New-ADGroup -Name "$groupName0" -GroupScope DomainLocal -GroupCategory Security -Path "ou=Locaux,ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
-           Write-Host "Creating $($groupName0)"
+           
         }
     }
 }
@@ -72,14 +78,14 @@ foreach ($partage in $partages)
     {
         foreach ($site in $sites) # DL-Partage-Site-Paris-CT LM L
         {
-            New-Item -path "c:\Partage\Sites" -Name "$site" -ItemType directory # création des sous dossier dans c:\
+            New-Item -path "c:\Partage\Sites" -Name "$site" -ItemType directory | Out-Null # création des sous dossier dans c:\
 
             foreach($rightType in $rightTypes)
             {
                $groupName1 = "DL-$($partage)-$($rootSite)-$($site)-$($rightType)"
                Write-Host $groupName1
                New-ADGroup -Name "$groupName1" -GroupScope DomainLocal -GroupCategory Security -Path "ou=Locaux,ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
-               Write-Host "Creating $($groupName1)"
+               
             }
         }
     }
@@ -94,14 +100,14 @@ foreach ($partage in $partages)
         {
             foreach($service in $services)
             {
-                New-Item -path "c:\Partage\Sites\$site" -Name "$service" -ItemType directory # création des sous dossier dans c:\
+                New-Item -path "c:\Partage\Sites\$site" -Name "$service" -ItemType directory | Out-Null # création des sous dossier dans c:\
 
                 foreach($rightType in $rightTypes)
                 {
                    $groupName2 = "DL-$($partage)-$($rootSite)-$($site)-$($service)-$($rightType)"
                    Write-Host $groupName2
                    New-ADGroup -Name "$groupName2" -GroupScope DomainLocal -GroupCategory Security -Path "ou=Locaux,ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
-                   Write-Host "Creating $($groupName2)"
+                   
                }
             }
         }
@@ -119,14 +125,14 @@ foreach ($partage in $partages)
                 foreach($folder in $folders)
                 {
 
-                New-Item -path "c:\Partage\Sites\$site\$service" -Name "$folder" -ItemType directory # création des sous dossier dans c:\
+                New-Item -path "c:\Partage\Sites\$site\$service" -Name "$folder" -ItemType directory | Out-Null # création des sous dossier dans c:\
 
                     foreach($rightType in $rightTypes)
                     {
                         $groupName3 = "DL-$($partage)-$($rootSite)-$($site)-$($service)-$($folder)-$($rightType)"
                         Write-Host $groupName3
                         New-ADGroup -Name "$groupName3" -GroupScope DomainLocal -GroupCategory Security -Path "ou=Locaux,ou=Groupes,ou=Sites,dc=$netbios,dc=$domain"
-                        Write-Host "Creating $($groupName3)"
+                       
                     }
                 }
             }
